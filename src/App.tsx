0a01d386e4ec659f4fdd6e4618defcba066cc95c
zl0i <dmitri.popov.96@mail.ru>
2023-12-23 10:37:10 +0300

    fix: route paths

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export default function App() {
   return (
     <>
       <header>
-        <Link to={`${import.meta.env.BASE_URL}/`}>Home</Link>
+        <Link to={`/`}>Home</Link>
         <Link to={`${import.meta.env.BASE_URL}/airbase`}>Airbases</Link>
         <Link to={`${import.meta.env.BASE_URL}/aircrafts`}>Aircrafts</Link>
         <Link to={`${import.meta.env.BASE_URL}/map`}>Map</Link>
@@ -39,10 +39,10 @@ export default function App() {
         <hr />
       </header>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/airbase" element={<AirbasesPage />}></Route>
-        <Route path="/aircrafts" element={<AircraftsPage />}></Route>
-        <Route path="/map" element={<MapPage />}></Route>
+        <Route path={`/`} element={<HomePage />}></Route>
+        <Route path={`${import.meta.env.BASE_URL}/airbase`} element={<AirbasesPage />}></Route>
+        <Route path={`${import.meta.env.BASE_URL}/aircrafts`} element={<AircraftsPage />}></Route>
+        <Route path={`${import.meta.env.BASE_URL}/map`} element={<MapPage />}></Route>
       </Routes>
     </>
   );
