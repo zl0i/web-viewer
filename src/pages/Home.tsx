c8a3a1cc00bf8c104b3004b5382707ed368b05e2
zl0i <dmitri.popov.96@mail.ru>
2023-12-23 12:27:37 +0300

    fix: change path to firebase-messaging-sw.js

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,18 @@ import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 export function HomePage() {
   if ('Notification' in window) {
     window.Notification.requestPermission()
-      .then(() => {
+      .then(() => navigator.serviceWorker.register(`${import.meta.env.BASE_URL}/firebase-messaging-sw.js`))
+      .then((registration) => {
         const messaging = getMessaging();
 
         onMessage(messaging, (payload) => {
           console.log(payload.notification?.title);
         });
 
-        return getToken(messaging, { vapidKey: 'BLguINrNTm3cFPY2wtx-7YaY1Y9_YDL6oxoxGn4BcDZm5EcnpsoXcnPAbnCc3kAuAvTpE1J1SQ__fxgG4SzsiJE' });
+        return getToken(messaging, {
+          vapidKey: 'BLguINrNTm3cFPY2wtx-7YaY1Y9_YDL6oxoxGn4BcDZm5EcnpsoXcnPAbnCc3kAuAvTpE1J1SQ__fxgG4SzsiJE',
+          serviceWorkerRegistration: registration,
+        });
       })
       .then((token) => {
         console.log(token);
