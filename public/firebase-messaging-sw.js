db96d7529759bddc803dad36ea213b55ddbd7142
zl0i <dmitri.popov.96@mail.ru>
2023-12-24 14:51:13 +0300

    fix: firebase background message

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,10 +18,10 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = payload.data.title;
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon || payload.notification.image,
+    body: payload.data.body,
+    icon: payload.data.icon || payload.data.image,
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
